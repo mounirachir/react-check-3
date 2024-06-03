8a247466d497d3ac569ec4b87cd1d92e2473e20c
mounir achir <mistamounir02@gmail.com>
2024-06-03 13:03:07 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Profile from "./Profile";
-
+import "./App.css";
 class App extends Component {
   state = {
     person: {
--- src/Profile.js
+++ src/Profile.js
@@ -1,15 +1,32 @@
 // Profile.js
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
-const Profile = ({ fullName, bio, imgSrc, profession }) => {
-  return (
-    <div className="App">
-      <h1>{fullName}</h1>
-      <img src={imgSrc} alt="Profile" />
-      <p>Bio: {bio}</p>
-      <p>Profession: {profession}</p>
-    </div>
-  );
-};
+
+class Profile extends Component {
+  state = {
+    timer: 0,
+  };
+  componentDidMount = () => {
+    this.timerID = setInterval(() => {
+      this.setState({ timer: this.state.timer + 1 });
+    }, 1000);
+  };
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  render() {
+    const { fullName, imgSrc, bio, profession } = this.props;
+    const { timer } = this.state;
+    return (
+      <div className="App">
+        <h1>{fullName}</h1>
+        <img src={imgSrc} alt="Profile" />
+        <p>Bio: {bio}</p>
+        <p>Profession: {profession}</p>
+        <h2>{timer}</h2>
+      </div>
+    );
+  }
+}
 
 export default Profile;
